ee4e474c9a220cf1fc4242576ed548b0b010870b
johnmuthua <caveomuthama@gmail.com>
2023-08-16 19:27:05 +0300

    Add styles

--- src/components/subscribe/Subscribe.tsx
+++ src/components/subscribe/Subscribe.tsx
@@ -1,10 +1,10 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Button, TextField, Typography } from '@mui/material';
 import './styles.css';
 
 const Subscribe = () => {
   return (
     <Box className="subscribe">
-      <Box>
+      <Box className="subscribe-container">
         <Typography
           variant="h4"
           sx={{
@@ -28,6 +28,40 @@ const Subscribe = () => {
         >
           Subscribe to our NewsLetter{' '}
         </Typography>
+        <Typography
+          variant="body1"
+          sx={{
+            fontFamily: 'Poppins',
+            fontWeight: '200',
+            textAlign: 'justify',
+          }}
+        >
+          Are you ready to supercharge your professional journey? Don't miss out
+          on exclusive insights, expert tips, and game-changing strategies
+          delivered right to your inbox through the GPOL Newsletter!
+        </Typography>
+        <Box
+          sx={{
+            boxShadow: 2,
+            marginTop: '1rem',
+          }}
+        >
+          <TextField fullWidth label="Enter Email Address" id="fullWidth" />
+        </Box>
+        <Box
+          sx={{
+            marginTop: '0.5rem',
+          }}
+        >
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: '#f60',
+            }}
+          >
+            Subscribe
+          </Button>
+        </Box>
       </Box>
     </Box>
   );
