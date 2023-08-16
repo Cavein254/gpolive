4c3834d04988d0efcca85202c802b13339ad620a
johnmuthua <caveomuthama@gmail.com>
2023-08-16 10:01:05 +0300

    Add changes

--- src/components/overlay/Overlay.tsx
+++ src/components/overlay/Overlay.tsx
@@ -2,6 +2,8 @@ import { Box, Stack } from '@mui/system';
 import { AiFillCloseCircle } from 'react-icons/ai';
 import { BsFan } from 'react-icons/bs';
 
+import { Typography } from '@mui/material';
+import { BiLogInCircle } from 'react-icons/bi';
 import { Link } from 'react-router-dom';
 import { HeaderLinks } from '../header/LinkData';
 import './styles.css';
@@ -34,7 +36,20 @@ const Overlay = ({ isOpen, setIsOpen }: Props) => {
         </Box>
         <Box className="overlay-links-wrapper">{links}</Box>
       </Box>
-      <Box></Box>
+      <Box className="overlay-bottom">
+        <BiLogInCircle className="overlay-icon" />
+        <Typography
+          variant="h5"
+          sx={{
+            fontFamily: 'Poppins',
+            fontWeight: '900',
+            color: '#ff6600',
+            fontSize: '2rem',
+          }}
+        >
+          Log Out
+        </Typography>
+      </Box>
     </Box>
   );
 };
